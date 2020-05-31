64826fa5e73f7720496a5bbdfd0475a6edced624
tvasuthevan123 <45975730+tvasuthevan123@users.noreply.github.com>
2020-05-31 14:51:44 +0100

    Slow Down prefab
    Created a prefab for the slow down perk

--- Project/Assets/Scripts/SlowDownPerk.cs
+++ Project/Assets/Scripts/SlowDownPerk.cs
@@ -1,25 +1,28 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SlowDownPerk : MonoBehaviour
 {
     public PlayerMovement player;
+
+    public float duration;
     // Update is called once per frame
     void OnTriggerEnter(Collider info)
     {
         if(info.CompareTag("Player"))
         {
-            slowMo();
+            StartCoroutine(slowMo());
         }
     }
 
     IEnumerator slowMo()
     {
+        Debug.Log("Player slowed");
         player.slowDown();
 
-        yield return new WaitForSeconds(3);
+        yield return new WaitForSeconds(duration);
 
+        Debug.Log("Player speed up");
         player.speedUp();
     }
 }
