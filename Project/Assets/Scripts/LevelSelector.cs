ed5c228a656a01b2f260139491d17455837acfac
tvasuthevan123 <45975730+tvasuthevan123@users.noreply.github.com>
2020-06-01 01:22:19 +0100

    Update LevelSelector.cs
    Made a more efficient event listener for the buttons to use the text from the button than a variable from memory.

--- Project/Assets/Scripts/LevelSelector.cs
+++ Project/Assets/Scripts/LevelSelector.cs
@@ -7,7 +7,6 @@ using UnityEngine.SceneManagement;
 public class LevelSelector : MonoBehaviour
 {
     public Button ButtonPrefab;
-    // private Button[] levels;
 
     public GridLayoutGroup grid;
     // Start is called before the first frame update
@@ -15,15 +14,17 @@ public class LevelSelector : MonoBehaviour
     {
         int progress = PlayerPrefs.GetInt("progress");
         int levelNumber = SceneManager.sceneCountInBuildSettings;
+        Debug.Log(levelNumber);  
         // levels = new Button[levelNumber];
         for(int i=2; i<levelNumber; i++)
         {
-            Button levelButton = Instantiate(ButtonPrefab);
-            levelButton.GetComponentInChildren<Text>().text = "|"+ (i-1) +"|";
-            levelButton.transform.SetParent(grid.transform, false);
             Debug.Log(i);
-            int level = i;
-            levelButton.onClick.AddListener(() => {SceneManager.LoadScene(level);Debug.Log(SceneManager.GetActiveScene().buildIndex);});
+            Button levelButton = Instantiate(ButtonPrefab);
+            levelButton.GetComponentInChildren<Text>().text = (i-1).ToString();
+            levelButton.transform.SetParent(grid.transform, false); 
+
+            //Figure out why this is the way it is.
+            levelButton.onClick.AddListener(() => {SceneManager.LoadScene(int.Parse(levelButton.GetComponentInChildren<Text>().text)+1);});
             if(!(i<=progress))
             levelButton.interactable = false;
         }
